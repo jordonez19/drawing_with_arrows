0a96941bf5785a65d13f16681305656484c5a10d
Javier <javier19pedraza@gmail.com>
2020-11-04 16:56:20 -0600

    actualizacion de teclas

--- dibujando.js
+++ dibujando.js
@@ -2,9 +2,48 @@ document.getElementById("area_dibujo");
 document.addEventListener("keyup", dibujarTeclado);
 
 
+
+var teclas = {
+
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+};
+
+
 /* FUNCTIONS */
 function dibujarTeclado(evento){
 
+    
     console.log("Tecla oprimida");
+    console.log(evento.keyCode);
 
 }
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+edad = 20
+teclas:
+
+left = 37
+up = 38
+right = 39
+down = 40
+
+
+
+
+*/
